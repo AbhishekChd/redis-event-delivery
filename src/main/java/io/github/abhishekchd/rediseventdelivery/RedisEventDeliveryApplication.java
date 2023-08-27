de52d1d4f75f9510cbd0823b6dece1711eb9e295
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2023-08-27 17:14:45 +0530

    Setup basic spring project

---
+++ src/main/java/io/github/abhishekchd/rediseventdelivery/RedisEventDeliveryApplication.java
@@ -0,0 +1,13 @@
+package io.github.abhishekchd.rediseventdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisEventDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisEventDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/abhishekchd/rediseventdelivery/RedisEventDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abhishekchd.rediseventdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisEventDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
