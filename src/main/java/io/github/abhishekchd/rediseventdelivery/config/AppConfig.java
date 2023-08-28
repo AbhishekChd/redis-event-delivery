cf5c5f03365e5b3c9430e0c7838381a7c96f4949
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2023-08-28 15:07:17 +0530

    Feature: Events retry and backoff 1. Events will be retried a fix number of tries and then deleted from persistent storage 2. Each retry is separated by an increasing delay calculated using exponential backoff algorithm 3. Retry count and fixed delay can be easily modified in configuration

--- src/main/java/io/github/abhishekchd/rediseventdelivery/config/AppConfig.java
+++ src/main/java/io/github/abhishekchd/rediseventdelivery/config/AppConfig.java
@@ -1,11 +1,28 @@
 package io.github.abhishekchd.rediseventdelivery.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+@Getter
 @Configuration
 @EnableAsync
-@EnableScheduling
+@EnableAutoConfiguration
 public class AppConfig {
+    @Value("${event_scheduler.retry.count}")
+    private Integer eventSchedulerRetryCount;
+
+    @Value("${event_scheduler.wait_duration}")
+    private Integer eventSchedulerWaitDuration;
+
+    @Bean
+    public ScheduledExecutorService scheduledExecutorService() {
+        return Executors.newScheduledThreadPool(5);
+    }
 }
--- src/main/java/io/github/abhishekchd/rediseventdelivery/scheduler/EventScheduler.java
+++ src/main/java/io/github/abhishekchd/rediseventdelivery/scheduler/EventScheduler.java
@@ -1,38 +1,66 @@
 package io.github.abhishekchd.rediseventdelivery.scheduler;
 
+import io.github.abhishekchd.rediseventdelivery.config.AppConfig;
 import io.github.abhishekchd.rediseventdelivery.data.EventRepository;
 import io.github.abhishekchd.rediseventdelivery.model.Event;
 import io.github.abhishekchd.rediseventdelivery.model.EventEntity;
 import io.github.abhishekchd.rediseventdelivery.publisher.EventPublisher;
 import io.github.abhishekchd.rediseventdelivery.util.BeanProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-
+@Slf4j
 public class EventScheduler {
+    private static final Random RANDOM = new Random();
     private static volatile EventScheduler sInstance = null;
-
+    private final int MAX_RETRY_COUNT;
+    private final int WAIT_DURATION;
     private final EventPublisher eventPublisher;
     private final EventRepository eventRepository;
+    private final ScheduledExecutorService scheduler;
     private final EventStateLifecycle<EventEntity> lifecycle = new EventStateLifecycle<>() {
         @Override
         public void onSuccess(@NonNull EventEntity entity) {
             // Remove from database
             eventRepository.deleteById(entity.getSequenceId());
+            log.info("Event {} successfully published in {} retries", entity.getSequenceId(), entity.getTryCount());
         }
 
         @Override
         public void onFailure(@NonNull EventEntity entity, @NonNull String message) {
-            // TODO: Change try_count and check for retry_count and apply back_off
             entity.setTryCount(entity.getTryCount() + 1);
-            eventRepository.save(entity);
+
+            // Remove event after "Max Retry Limit"
+            if (entity.getTryCount() > MAX_RETRY_COUNT) {
+                eventRepository.deleteById(entity.getSequenceId());
+                log.info("Event {} failed - {}. Retry limit of {} reached.", entity.getSequenceId(), message, MAX_RETRY_COUNT);
+
+            } else {
+                eventRepository.save(entity);
+                log.info("Event {} failed - {}. Pushing for retry.", entity.getSequenceId(), message);
+            }
         }
     };
 
     private EventScheduler() {
+        final int DEFAULT_MAX_RETRY_COUNT = 2;
+        int DEFAULT_WAIT_DURATION = 500; // 500ms
         this.eventPublisher = BeanProvider.getBean(EventPublisher.class);
         this.eventRepository = BeanProvider.getBean(EventRepository.class);
+        this.scheduler = BeanProvider.getBean(ScheduledExecutorService.class);
+
+        AppConfig appConfig = BeanProvider.getBean(AppConfig.class);
+
+        Integer retryCountValue = appConfig.getEventSchedulerRetryCount();
+        MAX_RETRY_COUNT = Objects.isNull(retryCountValue) ? DEFAULT_MAX_RETRY_COUNT : retryCountValue;
+
+        Integer waitDurationValue = appConfig.getEventSchedulerWaitDuration();
+        WAIT_DURATION = Objects.isNull(waitDurationValue) ? DEFAULT_WAIT_DURATION : waitDurationValue;
     }
 
     public static EventScheduler getInstance() {
@@ -47,16 +75,29 @@ public class EventScheduler {
     }
 
     public void scheduleEvent(EventEntity eventEntity) {
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        long delay = exponentialBackoffDelay(eventEntity.getTryCount());
+        scheduler.schedule(() -> executePublishEvent(eventEntity), delay, TimeUnit.MILLISECONDS);
+        log.info("Scheduled event {} with delay of {}", eventEntity.getSequenceId(), delay);
+    }
 
+    public void executePublishEvent(EventEntity eventEntity) {
         // Here we check for success/failure
         eventPublisher.publishMessage(Event.from(eventEntity));
 
-        // For now considering success
+        // For now considering success. We can verify retry/back-off by setting this to onFailure()
         lifecycle.onSuccess(eventEntity);
+    }
+
+    /**
+     * Attempting an exponential backoff using a constant wait duration for each request.
+     * WAIT_DURATION = w
+     * Attempts      = n
+     * backoff_delay = (w * 2^n) +- small_random_delay
+     *
+     * @param attempts Number of attempts already made to execute command
+     * @return delay in milliseconds
+     */
+    private long exponentialBackoffDelay(int attempts) {
+        return WAIT_DURATION * (1L << attempts) + RANDOM.nextLong(-9, 10);
     }
 }
